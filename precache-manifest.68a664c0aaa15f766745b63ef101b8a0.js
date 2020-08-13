3e168b9d5f7c4bbb6605d3b7816483b9bbd71f9d
CYMAD <40774917+CYMAD@users.noreply.github.com>
2020-08-13 13:28:07 +0300

    Updates

---
+++ precache-manifest.68a664c0aaa15f766745b63ef101b8a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "622fcd495c5fe4705451feb45a0e08ff",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "a7ddd5cf82f797f9a00d",
+    "url": "/corona-tracker/static/css/main.6557891a.chunk.css"
+  },
+  {
+    "revision": "a6783a0f95f871f6fc97",
+    "url": "/corona-tracker/static/js/2.346d03b7.chunk.js"
+  },
+  {
+    "revision": "409b74bb8bb24cd1886ec902914e01e2",
+    "url": "/corona-tracker/static/js/2.346d03b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7ddd5cf82f797f9a00d",
+    "url": "/corona-tracker/static/js/main.97fe4595.chunk.js"
+  },
+  {
+    "revision": "9c4d7b1cf1de7ae99f79",
+    "url": "/corona-tracker/static/js/runtime-main.bf840863.js"
+  },
+  {
+    "revision": "be3acb5e8bf6ce91d055328aaa6a4802",
+    "url": "/corona-tracker/static/media/red-circle.be3acb5e.png"
+  }
+]);
